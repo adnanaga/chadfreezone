16033f520fb0428d0ba9c235eaeafd979a579f1e
James Gualtieri <jgualtieri2310@gmail.com>
2020-05-28 21:28:41 -0700

    add custom name list, display user preferences

--- filter.js
+++ filter.js
@@ -1,5 +1,4 @@
 function save_options() {
-  
     if (document.getElementById('auto').checked) {
         chrome.storage.sync.set({
           s4m: true
@@ -12,15 +11,23 @@ function save_options() {
 
     if (document.getElementById('male').checked) {
       chrome.storage.sync.set({
-        terribleNames: ["Chad", "Zachary", "Kyle"]
+        terribleNames: ["Chad", "Zachary", "Kyle"],
+        selection: 'male-default'
       });
     } else if (document.getElementById('female').checked) {
       chrome.storage.sync.set({
-        terribleNames: ['Mariah','Jessica', 'Jess', 'Emily', 'Alex', 'Emma', 'Sarah']
+        terribleNames: ['Mariah','Jessica', 'Jess', 'Emily', 'Alex', 'Emma', 'Sarah'],
+        selection: 'female-default'
       });
     } else if (document.getElementById('both').checked) {
       chrome.storage.sync.set({
-        terribleNames: ["Chad", "Zachary", "Kyle", 'Mariah','Jessica', 'Jess', 'Emily', 'Alex' ]
+        terribleNames: ["Chad", "Zachary", "Kyle", 'Mariah','Jessica', 'Jess', 'Emily', 'Alex' ],
+        selection: 'both-default'
+      });
+    } else if (document.getElementById('custom').checked) {
+      chrome.storage.sync.set({
+        selection: 'custom',
+        terribleNames: get_names()
       });
     }
     chrome.tabs.executeScript({
@@ -31,3 +38,47 @@ function save_options() {
 }
 
 document.getElementById('go').addEventListener('click', save_options);
+
+function get_names() {
+    // custom user inputed names
+    var names = document.getElementById('terrible_names').value.replace(/\s/g,'');
+    var terribleNames = names.split(',');
+    return terribleNames;
+}
+
+document.getElementById('custom').addEventListener('click', () => {
+    document.getElementById('names-elem').className = 'dropdown-content-open'; 
+});
+
+function display_options() {
+    chrome.storage.sync.get(["selection", "terribleNames", "s4m"], function(result) {
+        selection = result.selection;
+        terribleNames = result.terribleNames;
+        s4m = result.s4m;
+
+        if (selection === undefined ) return;
+
+        switch(selection){
+            case 'male-default':
+                document.getElementById('male').checked = true;
+                break;
+            case 'female-default':
+                document.getElementById('female').checked = true;
+                break;
+            case 'both-default':
+                document.getElementById('both').checked = true;
+                break;
+            case 'custom':
+                document.getElementById('custom').checked = true;
+                document.getElementById('names-elem').className = 'dropdown-content-open';
+                document.getElementById('terrible_names').value = terribleNames.toString();
+                break;
+            default:
+                break;
+        }
+        document.getElementById('auto').checked = s4m;
+        document.getElementById('user').checked = !s4m;
+    });
+}
+
+window.onload = display_options()
